fa8b5cd7750ce1a198b1c8655f77e2ee35aa8817
David Kettler <21echoes@gmail.com>
2015-12-16 11:58:50 -0800

    paginate the pledges to the campaign

--- src/main/java/com/patreon/API.java
+++ src/main/java/com/patreon/API.java
@@ -10,6 +10,7 @@ import java.net.MalformedURLException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.net.URLEncoder;
 
 public class API {
   private final String _accessToken;
@@ -28,6 +29,19 @@ public class API {
 
   public JSONObject fetchCampaign() {
     return this.getJSON("current_user/campaigns?include=rewards,creator,goals");
+  }
+
+  public JSONObject fetchPageOfPledges(String campaignID, int pageSize, String cursor) {
+    String url = String.format("campaigns/%s/pledges?page%5Bcount%5D=%s", campaignID, pageSize);
+    if (cursor != null) {
+      try {
+        String escapedCursor = URLEncoder.encode(cursor, "UTF-8");
+        url.concat(String.format("&page%5Bcursor%5D=%s", escapedCursor));
+      } catch (java.io.UnsupportedEncodingException e) {
+        System.err.println("UnsupportedEncodingException: " + e.getMessage());
+      }
+    }
+    return this.getJSON(url);
   }
 
   private JSONObject getJSON(String suffix) {
