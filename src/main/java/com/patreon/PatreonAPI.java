b7f6157bd1cdc664cb45af1f4472ec5d4c6e2597
Joshua Hull <joshbuddy@gmail.com>
2018-02-06 10:52:30 -0800

    Re-format of all code (#11)

--- src/main/java/com/patreon/PatreonAPI.java
+++ src/main/java/com/patreon/PatreonAPI.java
@@ -14,155 +14,158 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class PatreonAPI {
-    private final String accessToken;
-    private ResourceConverter converter;
-    private static final Logger LOG = LoggerFactory.getLogger(PatreonAPI.class);
+  private static final Logger LOG = LoggerFactory.getLogger(PatreonAPI.class);
+  private final String accessToken;
+  private ResourceConverter converter;
 
-    /**
-     * Create a new instance of the Patreon API. You only need <b>one</b> of these objects unless you are using the API with multiple tokens
-     *
-     * @param accessToken The "Creator's Access Token" found on <a href="https://www.patreon.com/platform/documentation/clients">the patreon client page</a> <b>OR</b> OAuth access token
-     */
-    public PatreonAPI(String accessToken) {
-        this.accessToken = accessToken;
+  /**
+   * Create a new instance of the Patreon API. You only need <b>one</b> of these objects unless you are using the API with multiple tokens
+   *
+   * @param accessToken The "Creator's Access Token" found on <a href="https://www.patreon.com/platform/documentation/clients">the patreon client page</a> <b>OR</b> OAuth access token
+   */
+  public PatreonAPI(String accessToken) {
+    this.accessToken = accessToken;
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-        this.converter = new ResourceConverter(
-            objectMapper,
-            User.class,
-            Campaign.class,
-            Pledge.class
-        );
-        this.converter.enableDeserializationOption(DeserializationFeature.ALLOW_UNKNOWN_INCLUSIONS);
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    this.converter = new ResourceConverter(
+                                            objectMapper,
+                                            User.class,
+                                            Campaign.class,
+                                            Pledge.class
+    );
+    this.converter.enableDeserializationOption(DeserializationFeature.ALLOW_UNKNOWN_INCLUSIONS);
+  }
+
+  /**
+   * Get the user object of the creator
+   *
+   * @return JSONAPIDocument<User> containing all data pertaining to the current user
+   * @throws IOException Thrown when the GET request failed
+   */
+  public JSONAPIDocument<User> fetchUser() throws IOException {
+    String path = new URIBuilder()
+                    .setPath("current_user")
+                    .addParameter("include", "pledges")
+                    .toString();
+    return converter.readDocument(
+      getDataStream(path),
+      User.class
+    );
+  }
+
+  /**
+   * Get a list of campaigns the current creator is running - also contains other related data like Goals
+   * Note: The first campaign data object is located at index 0 in the data list
+   *
+   * @return JSONAPIDocument<List<Campaign>> containing the above-mentioned data
+   * @throws IOException Thrown when the GET request failed
+   */
+  public JSONAPIDocument<List<Campaign>> fetchCampaigns() throws IOException {
+    String path = new URIBuilder()
+                    .setPath("current_user/campaigns")
+                    .addParameter("include", "rewards,creator,goals")
+                    .toString();
+    return converter.readDocumentCollection(
+      getDataStream(path),
+      Campaign.class
+    );
+  }
+
+  /**
+   * Retrieve pledges for the specified campaign
+   *
+   * @param campaignId id for campaign to retrieve
+   * @param pageSize   how many pledges to return
+   * @param pageCursor ignore, put null.
+   * @return JSONAPIDocument<List<Pledge>> containing pledges & associated data
+   * @throws IOException Thrown when the GET request failed
+   */
+  public JSONAPIDocument<List<Pledge>> fetchPageOfPledges(String campaignId, int pageSize, String pageCursor) throws IOException {
+    URIBuilder pathBuilder = new URIBuilder()
+                               .setPath(String.format("campaigns/%s/pledges", campaignId))
+                               .addParameter("page[count]", String.valueOf(pageSize));
+    if (pageCursor != null) {
+      pathBuilder.addParameter("page[cursor]", pageCursor);
     }
+    return converter.readDocumentCollection(
+      getDataStream(pathBuilder.toString()),
+      Pledge.class
+    );
+  }
 
-    /**
-     * Get the user object of the creator
-     *
-     * @return JSONAPIDocument<User> containing all data pertaining to the current user
-     * @throws IOException Thrown when the GET request failed
-     */
-    public JSONAPIDocument<User> fetchUser() throws IOException {
-        String path = new URIBuilder()
-            .setPath("current_user")
-            .addParameter("include", "pledges")
-            .toString();
-        return converter.readDocument(
-            getDataStream(path),
-            User.class
-        );
+  public String getNextCursorFromDocument(JSONAPIDocument document) {
+    Links links = document.getLinks();
+    if (links == null) {
+      return null;
     }
-
-    /**
-     * Get a list of campaigns the current creator is running - also contains other related data like Goals
-     * Note: The first campaign data object is located at index 0 in the data list
-     *
-     * @return JSONAPIDocument<List<Campaign>> containing the above-mentioned data
-     * @throws IOException Thrown when the GET request failed
-     */
-    public JSONAPIDocument<List<Campaign>> fetchCampaigns() throws IOException {
-        String path = new URIBuilder()
-            .setPath("current_user/campaigns")
-            .addParameter("include", "rewards,creator,goals")
-            .toString();
-        return converter.readDocumentCollection(
-            getDataStream(path),
-            Campaign.class
-        );
+    Link nextLink = links.getNext();
+    if (nextLink == null) {
+      return null;
     }
-
-    /**
-     * Retrieve pledges for the specified campaign
-     *
-     * @param campaignId id for campaign to retrieve
-     * @param pageSize   how many pledges to return
-     * @param pageCursor ignore, put null.
-     * @return JSONAPIDocument<List<Pledge>> containing pledges & associated data
-     * @throws IOException Thrown when the GET request failed
-     */
-    public JSONAPIDocument<List<Pledge>> fetchPageOfPledges(String campaignId, int pageSize, String pageCursor) throws IOException {
-        URIBuilder pathBuilder = new URIBuilder()
-            .setPath(String.format("campaigns/%s/pledges", campaignId))
-            .addParameter("page[count]", String.valueOf(pageSize));
-        if (pageCursor != null) {
-            pathBuilder.addParameter("page[cursor]", pageCursor);
+    String nextLinkString = nextLink.toString();
+    try {
+      List<NameValuePair> queryParameters = URLEncodedUtils.parse(new URI(nextLinkString), "utf8");
+      for (NameValuePair pair : queryParameters) {
+        String name = pair.getName();
+        if (name.equals("page[cursor]")) {
+          String cursorValue = pair.getValue();
+          return cursorValue;
         }
-        return converter.readDocumentCollection(
-            getDataStream(pathBuilder.toString()),
-            Pledge.class
-        );
+      }
+    } catch (URISyntaxException e) {
+      LOG.error(e.getMessage());
     }
+    return null;
+  }
 
-    public String getNextCursorFromDocument(JSONAPIDocument document) {
-        Links links = document.getLinks();
-        if (links == null) {
-            return null;
-        }
-        Link nextLink = links.getNext();
-        if (nextLink == null) {
-            return null;
-        }
-        String nextLinkString = nextLink.toString();
-        try {
-            List<NameValuePair> queryParameters = URLEncodedUtils.parse(new URI(nextLinkString), "utf8");
-            for (NameValuePair pair : queryParameters) {
-                String name = pair.getName();
-                if (name.equals("page[cursor]")) {
-                    String cursorValue = pair.getValue();
-                    return cursorValue;
-                }
-            }
-        } catch (URISyntaxException e) {
-            LOG.error(e.getMessage());
-        }
-        return null;
+  public List<Pledge> fetchAllPledges(String campaignId) throws IOException {
+    Set<Pledge> pledges = new HashSet<>();
+    String cursor = null;
+    while (true) {
+      JSONAPIDocument<List<Pledge>> pledgesPage = fetchPageOfPledges(campaignId, 15, cursor);
+      pledges.addAll(pledgesPage.get());
+      cursor = getNextCursorFromDocument(pledgesPage);
+      if (cursor == null) {
+        break;
+      }
     }
+    return new ArrayList<>(pledges);
+  }
 
-    public List<Pledge> fetchAllPledges(String campaignId) throws IOException {
-        Set<Pledge> pledges = new HashSet<>();
-        String cursor = null;
-        while (true) {
-            JSONAPIDocument<List<Pledge>> pledgesPage = fetchPageOfPledges(campaignId, 15, cursor);
-            pledges.addAll(pledgesPage.get());
-            cursor = getNextCursorFromDocument(pledgesPage);
-            if (cursor == null) {
-                break;
-            }
-        }
-        return new ArrayList<>(pledges);
+  private InputStream getDataStream(String suffix) throws IOException {
+    try {
+      String prefix = "https://www.patreon.com/api/oauth2/api/";
+      URL url = new URL(prefix.concat(suffix));
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestProperty("Authorization", "Bearer ".concat(this.accessToken));
+      connection.setRequestProperty("User-Agent",
+        String.format(
+          "Patreon-Java, version %s, platform %s %s",
+          getVersion(),
+          System.getProperty("os.name"),
+          System.getProperty("os.version")));
+      return connection.getInputStream();
+    } catch (IOException e) {
+      LOG.error(e.getMessage());
+      throw e;
     }
+  }
 
-    private InputStream getDataStream(String suffix) throws IOException {
-        try {
-            String prefix = "https://www.patreon.com/api/oauth2/api/";
-            URL url = new URL(prefix.concat(suffix));
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Authorization", "Bearer ".concat(this.accessToken));
-            connection.setRequestProperty("User-Agent",
-                String.format(
-                    "Patreon-Java, version %s, platform %s %s",
-                    getVersion(),
-                    System.getProperty("os.name"),
-                    System.getProperty("os.version")));
-            return connection.getInputStream();
-        } catch (IOException e) {
-            LOG.error(e.getMessage());
-            throw e;
-        }
-    }
-
-    private String getVersion() throws IOException {
-        InputStream resourceAsStream = this.getClass().getResourceAsStream("/version.properties");
-        java.util.Properties prop = new java.util.Properties();
-        prop.load(resourceAsStream);
-        return prop.getProperty("version");
-    }
+  private String getVersion() throws IOException {
+    InputStream resourceAsStream = this.getClass().getResourceAsStream("/version.properties");
+    java.util.Properties prop = new java.util.Properties();
+    prop.load(resourceAsStream);
+    return prop.getProperty("version");
+  }
 }
--- src/main/java/com/patreon/PatreonOAuth.java
+++ src/main/java/com/patreon/PatreonOAuth.java
@@ -7,105 +7,105 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Calendar;
-import java.util.Date;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Calendar;
+import java.util.Date;
 
 public class PatreonOAuth {
-    private final String clientID;
-    private final String clientSecret;
-    private final String redirectUri;
-    private static final Gson gson = new GsonBuilder().serializeNulls().enableComplexMapKeySerialization().create();
-    private static final Logger LOG = LoggerFactory.getLogger(PatreonOAuth.class);
-    private static final String GRANT_TYPE_AUTHORIZATION = "authorization_code";
-    private static final String GRANT_TYPE_TOKEN_REFRESH = "token_refresh";
+  private static final Gson gson = new GsonBuilder().serializeNulls().enableComplexMapKeySerialization().create();
+  private static final Logger LOG = LoggerFactory.getLogger(PatreonOAuth.class);
+  private static final String GRANT_TYPE_AUTHORIZATION = "authorization_code";
+  private static final String GRANT_TYPE_TOKEN_REFRESH = "token_refresh";
+  private final String clientID;
+  private final String clientSecret;
+  private final String redirectUri;
 
-    public PatreonOAuth(String clientID, String clientSecret, String redirectUri) {
-        this.clientID = clientID;
-        this.clientSecret = clientSecret;
-        this.redirectUri = redirectUri;
+  public PatreonOAuth(String clientID, String clientSecret, String redirectUri) {
+    this.clientID = clientID;
+    this.clientSecret = clientSecret;
+    this.redirectUri = redirectUri;
+  }
+
+  private static <E> E toObject(String str, Class<E> clazz) {
+    return gson.fromJson(str, clazz);
+  }
+
+  public String getAuthorizationURL() {
+    URIBuilder builder = null;
+    try {
+      builder = new URIBuilder("https://www.patreon.com/oauth2/authorize");
+    } catch (URISyntaxException e) {
+      LOG.error(e.getMessage());
+    }
+    builder.addParameter("response_type", "code");
+    builder.addParameter("client_id", clientID);
+    builder.addParameter("redirect_uri", redirectUri);
+    return builder.toString();
+  }
+
+  public TokensResponse getTokens(String code) throws IOException {
+    Connection requestInfo = Jsoup.connect("https://www.patreon.com/api/oauth2/token")
+                               .data("grant_type", GRANT_TYPE_AUTHORIZATION)
+                               .data("code", code)
+                               .data("client_id", clientID)
+                               .data("client_secret", clientSecret)
+                               .data("redirect_uri", redirectUri)
+                               .ignoreContentType(true);
+    String response = requestInfo.post().body().text();
+
+    return toObject(response, TokensResponse.class);
+  }
+
+  public TokensResponse refreshTokens(String refreshToken) throws IOException {
+    Connection requestInfo = Jsoup.connect("https://www.patreon.com/api/oauth2/token")
+                               .data("grant_type", GRANT_TYPE_TOKEN_REFRESH)
+                               .data("client_id", clientID)
+                               .data("client_secret", clientSecret)
+                               .data("refresh_token", refreshToken)
+                               .ignoreContentType(true);
+    String response = requestInfo.post().body().text();
+    return toObject(response, TokensResponse.class);
+  }
+
+  public static class TokensResponse {
+    private String access_token;
+    private String refresh_token;
+    private int expires_in;
+    private String scope;
+    private String token_type;
+    private Date expiresAt;
+
+    public TokensResponse(String access_token, String refresh_token, int expires_in, String scope, String token_type) {
+      this.access_token = access_token;
+      this.refresh_token = refresh_token;
+      this.expires_in = expires_in;
+      this.scope = scope;
+      this.token_type = token_type;
+      Calendar calendar = Calendar.getInstance(); // gets a calendar using the default time zone and locale.
+      calendar.add(Calendar.SECOND, expires_in);
+      this.expiresAt = calendar.getTime();
     }
 
-    public String getAuthorizationURL() {
-        URIBuilder builder = null;
-        try {
-            builder = new URIBuilder("https://www.patreon.com/oauth2/authorize");
-        } catch (URISyntaxException e) {
-            LOG.error(e.getMessage());
-        }
-        builder.addParameter("response_type", "code");
-        builder.addParameter("client_id", clientID);
-        builder.addParameter("redirect_uri", redirectUri);
-        return builder.toString();
+    public String getAccessToken() {
+      return access_token;
     }
 
-    public TokensResponse getTokens(String code) throws IOException {
-        Connection requestInfo = Jsoup.connect("https://www.patreon.com/api/oauth2/token")
-            .data("grant_type", GRANT_TYPE_AUTHORIZATION)
-            .data("code", code)
-            .data("client_id", clientID)
-            .data("client_secret", clientSecret)
-            .data("redirect_uri", redirectUri)
-            .ignoreContentType(true);
-        String response = requestInfo.post().body().text();
-
-        return toObject(response, TokensResponse.class);
+    public String getRefreshToken() {
+      return refresh_token;
     }
 
-    public TokensResponse refreshTokens(String refreshToken) throws IOException {
-        Connection requestInfo = Jsoup.connect("https://www.patreon.com/api/oauth2/token")
-            .data("grant_type", GRANT_TYPE_TOKEN_REFRESH)
-            .data("client_id", clientID)
-            .data("client_secret", clientSecret)
-            .data("refresh_token", refreshToken)
-            .ignoreContentType(true);
-        String response = requestInfo.post().body().text();
-        return toObject(response, TokensResponse.class);
+    public int getExpiresIn() {
+      return expires_in;
     }
 
-    public static class TokensResponse {
-        private String access_token;
-        private String refresh_token;
-        private int expires_in;
-        private String scope;
-        private String token_type;
-        private Date expiresAt;
-
-        public TokensResponse(String access_token, String refresh_token, int expires_in, String scope, String token_type) {
-            this.access_token = access_token;
-            this.refresh_token = refresh_token;
-            this.expires_in = expires_in;
-            this.scope = scope;
-            this.token_type = token_type;
-            Calendar calendar = Calendar.getInstance(); // gets a calendar using the default time zone and locale.
-            calendar.add(Calendar.SECOND, expires_in);
-            this.expiresAt = calendar.getTime();
-        }
-
-        public String getAccessToken() {
-            return access_token;
-        }
-
-        public String getRefreshToken() {
-            return refresh_token;
-        }
-
-        public int getExpiresIn() {
-            return expires_in;
-        }
-
-        public String getScope() {
-            return scope;
-        }
-
-        public String getTokenType() {
-            return token_type;
-        }
+    public String getScope() {
+      return scope;
     }
 
-    private static <E> E toObject(String str, Class<E> clazz) {
-        return gson.fromJson(str, clazz);
+    public String getTokenType() {
+      return token_type;
     }
+  }
 }
